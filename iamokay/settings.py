99d7d49e1aada0042e997265ebde3eb8af6c3354
Syakhisk Al-Azmi <63556086+Syakhisk@users.noreply.github.com>
2020-06-02 20:08:25 +0700

    push for carousel

--- iamokay/settings.py
+++ iamokay/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'homepage',
+    # 'djangocms_slick_slider.apps.DjangocmsSlickSliderConfig',
 ]
 
 MIDDLEWARE = [
