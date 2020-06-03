8be6743d74194d53f84bed283b0c31749d2d79dc
Syakhisk Al-Azmi <63556086+Syakhisk@users.noreply.github.com>
2020-06-03 18:58:26 +0700

    Styling carousel, add responsive

--- homepage/static/js/slicker.js
+++ homepage/static/js/slicker.js
@@ -7,5 +7,29 @@ $(function () {
         dots : true,
         autoplay : true,
         autoplaySpeed : 2000,
+        
+        centerMode: true,
+        centerPadding: '60px',
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: true,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 3
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              arrows: true,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 1
+            }
+          }
+        ]
+        
     })
 })
